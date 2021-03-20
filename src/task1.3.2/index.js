615f80e3709ccff10f85b42500a91a7dd555afa1
aidfromdeagland <aidfromdeagland@gmail.com>
2021-03-20 12:00:44 +0300

    implement task 1.3

---
+++ src/task1.3.2/index.js
@@ -0,0 +1,87 @@
+'use strict';
+
+import path from 'path';
+import fs from 'fs';
+import fsPromises from 'fs/promises';
+import { pipeline } from 'stream';
+import csvToJson from 'csvtojson';
+
+const csvFilePath = path.join(__dirname, '/csv/nodejs-hw1-ex1.csv');
+const jsonFilePath = path.join(__dirname, '/json/nodejs-hw1-ex1.json');
+const txtFilePath = path.join(__dirname, '/txt/nodejs-hw1-ex1.txt');
+
+const errorHandler = (error) => {
+    console.error(error.message);
+};
+
+const checkPathExistance = (path) => {
+    return fsPromises.access(path)
+        .then(() => true)
+        .catch(() => false);
+};
+
+const truncateFile = (filePath) => {
+    return fsPromises.truncate(filePath, 0)
+        .catch((err) => errorHandler(err));
+};
+
+const prepareOutputforConvertation = (outputPath) => {
+    const outputDirectory = path.dirname(outputPath);
+
+    return checkPathExistance(outputDirectory)
+    .then((doesPathExist) => {
+        if (doesPathExist) {
+            return checkPathExistance(outputPath);
+        } else {
+            return fsPromises.mkdir(outputDirectory, { recursive: true })
+            .then(() => checkPathExistance(outputPath));
+        }
+    })
+    .then((doesFileExist) => {
+        if (doesFileExist) {
+            return truncateFile(outputPath);
+        }
+    })
+    .catch((err) => errorHandler(err));
+};
+
+const convertCsvToJson = (csvPath, jsonPath) => {
+    return  csvToJson()
+    .fromFile(csvPath)
+    .then((json) => {
+        return fsPromises.writeFile(jsonPath, JSON.stringify(json, null, 2));
+    })
+    .catch((err) => errorHandler(err));
+};
+
+const convertCsvToTxtLineByLine = (csvPath, txtPath) => {
+    pipeline(
+        csvToJson()
+        .fromFile(csvPath)
+        .subscribe((jsonLine) => {
+            return new Promise((resolve) => {
+                resolve(jsonLine);
+            })
+        })
+        .on('error', (err) => {
+            errorHandler(err);
+        }),
+        fs.createWriteStream(txtPath),
+        (err) => {
+            if (err) {
+                errorHandler(err);
+            }
+        }
+    );
+};
+
+const convertAccordingToTask = (inputPath, outputPath, handler) => {
+    prepareOutputforConvertation(outputPath)
+    .then(() => {
+        return handler(inputPath, outputPath);
+    })
+    .catch((err) => errorHandler(err));
+}
+
+convertAccordingToTask(csvFilePath, jsonFilePath, convertCsvToJson);
+convertAccordingToTask(csvFilePath, txtFilePath, convertCsvToTxtLineByLine);
